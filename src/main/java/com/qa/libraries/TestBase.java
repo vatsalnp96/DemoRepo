11db60b1624df473a2530665bc240975d6191ddb
vatsalnp96 <vatsalnp54@gmail.com>
2022-12-24 10:54:47 -0500

    First git commit for my selenium code

---
+++ src/main/java/com/qa/libraries/TestBase.java
@@ -0,0 +1,5 @@
+package com.qa.libraries;
+
+public class TestBase {
+
+}
---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+import com.qa.libraries.TestBase;
+
+public class LoginTest extends TestBase{
+	
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+
+}
